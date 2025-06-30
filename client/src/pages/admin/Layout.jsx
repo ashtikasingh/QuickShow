a9e997f364e03dc705ab47d9fc1923ad06f6b06c
Ashtika <aashtika2002@gmail.com>
2025-07-01 00:52:11 +0530

    corected the value

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -9,7 +9,7 @@ const Layout = () => {
             <AdminNavbar />
             <div>
                 <AdminSidebar />
-                <div className='flex-1 px-4 py-10 md:px-10 h-[calc(100vh-640px)] overflow-y-auto'>
+                <div className='flex-1 px-4 py-10 md:px-10 h-[calc(100vh-64px)] overflow-y-auto'>
                     <Outlet />
                 </div>
             </div>
