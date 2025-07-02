73a5c83407bcb4cb2f1405a879c667bf9e1c3467
Ashtika <aashtika2002@gmail.com>
2025-07-02 23:28:14 +0530

    updated admin dashboard

--- client/src/pages/admin/ListShows.jsx
+++ client/src/pages/admin/ListShows.jsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyShowsData } from '../../assets/assets';
 
 const ListShows = () => {
+    const currency = import.meta.env.VITE_CURRENCY
+
+    const [shows, setShows] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const getAllShows = async () => {
+        try {
+            setShows([{
+                movie: dummyShowsData[0],
+                showDateTime: "2025-06-30T02:30:00.000Z",
+                showPrice: 59,
+                occupiedSeats: {
+                    A1: "user_1",
+                    B1: "user_2",
+                    C1: "user_3"
+                }
+            }]);
+            setLoading(false);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        getAllShows();
+    }, []);
+
     return (
         <div>ListShows</div>
     )
