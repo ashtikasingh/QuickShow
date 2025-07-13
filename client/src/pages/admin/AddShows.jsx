0b8a4f51a05fb4fb2d669983bc55bb2572cc4883
Ashtika <aashtika2002@gmail.com>
2025-07-13 22:36:32 +0530

    updated values

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -9,9 +9,9 @@ const AddShows = () => {
     const currency = import.meta.env.VITE_CURRENCY
     const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
     const [selectedMovie, setSelectedMovie] = useState(null);
-    const [dateTimeSelection, setDateTimeSelection] = useState({});
-    const [dateTimeInput, setDateTimeInput] = useState("");
-    const [showPrice, setShowPrice] = useState("");
+    // const [dateTimeSelection, setDateTimeSelection] = useState({});
+    // const [dateTimeInput, setDateTimeInput] = useState("");
+    // const [showPrice, setShowPrice] = useState("");
 
     const fetchNowPlayingMovies = async () => {
         setNowPlayingMovies(dummyShowsData)
